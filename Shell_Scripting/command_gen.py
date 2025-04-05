895010ade0d360fc238244336ce740d33d81300d
Jaxon Avena <jaxonavena@gmail.com>
2025-04-05 15:11:49 -0500

    newlines and type check

--- Shell_Scripting/command_gen.py
+++ Shell_Scripting/command_gen.py
@@ -16,13 +16,13 @@ class CommandGen:
         script.write("#!/bin/bash\n")
         script.write("echo 'Setting up your environment...'\n")
 
-        script.write("sudo apt-get update")
+        script.write("sudo apt-get update\n")
 
-        if self.type == "system":
+        if self.dep["system_packages"]:
           for item in self.dep:
-            script.write(f"sudo apt-get install -y {item}")
+            script.write(f"sudo apt-get install -y {item}\n")
 
-        elif self.type == "lang":
+        elif self.dep["name"] and self.dep["version"]:
           self.interp = f"{self.dep["name"]}{self.dep["version"]}"
           script.write(f"sudo apt-get install -y {self.interp}")
           packs = self.dep["packages"]
@@ -31,5 +31,6 @@ class CommandGen:
           # requests==2.25.1 flask==2.0.1
           for pack in packs:
             command += f" {pack}"
+          command += "\n"
           script.write(command)
 
