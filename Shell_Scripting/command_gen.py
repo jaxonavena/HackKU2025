ee465c9b6081db6265065f8ac02556421b5f7cf4
Jaxon Avena <jaxonavena@gmail.com>
2025-04-05 16:12:30 -0500

    fix node

--- Shell_Scripting/command_gen.py
+++ Shell_Scripting/command_gen.py
@@ -13,21 +13,24 @@ class CommandGen:
       script.write("sudo apt-get update\n")
 
   def generate(self, dep):
+    system_package = dep.get("system_packages")
+    name = dep.get("name")
+    version = dep.get("version")
+
     with open("setup.sh", "a") as script:
-      if dep.get("system_packages"):
+      if system_package:
         for item in dep:
           script.write(f"sudo apt-get install -y {item}\n")
 
-      elif dep.get("name") and dep.get("version"):
-        interp = f"{dep.get("name")}{dep.get("version")}"
+      elif name and version:
+        interp = f"{dep.get("name")}{dep.get("version")} " if name == "python" else ""
         script.write(f"sudo apt-get install -y {interp}\n")
 
         packs = dep.get("packages")
-        command = f"{interp} {CommandGen.INSTALL_COMMANDS[dep.get("name")]}"
+        command = f"{interp}{CommandGen.INSTALL_COMMANDS[dep.get("name")]}"
 
         for pack in packs:
           command += f" {pack}"
         command += "\n"
-        print(command)
         script.write(command)
 
