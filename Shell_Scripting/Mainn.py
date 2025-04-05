b234ab4fa8e232ce67b045492231a1db665850f9
Pete Junge <petejunge77@Petes-MacBook-Pro-10.local>
2025-04-05 00:08:48 -0500

    buttsss

---
+++ Shell_Scripting/Mainn.py
@@ -0,0 +1,22 @@
+import json 
+import Parser from Parser
+
+## Add code for receiveing json file and replace this hard coded input
+file = pass
+# with open("{file}}.json", "r") as dependencies_file:
+#     deps = json.load(dependencies_file)
+parsed_data = Parser.parse(file)
+
+
+with open("setup.sh", "w") as script:
+    script.write("#!/bin/bash\n")
+    script.write("echo 'Setting up your environment...'\n")
+
+    
+    ## pip
+    match data:
+        case d
+
+    ## npm 
+butts
+
